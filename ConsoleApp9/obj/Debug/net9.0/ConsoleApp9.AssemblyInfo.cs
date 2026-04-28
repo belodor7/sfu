362d66ec48656eb0300ea9e0ae0c3be037abdcd2
Kirill.Britov <britov.k@mail.ru>
2026-04-28 22:02:48 +0700

    фффф

--- ConsoleApp9/obj/Debug/net9.0/ConsoleApp9.AssemblyInfo.cs
+++ ConsoleApp9/obj/Debug/net9.0/ConsoleApp9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd86a0888546717aab4f5eac948e54b79adf3de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4afaf645709720ccd35d31fc386b9d8564f1cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
